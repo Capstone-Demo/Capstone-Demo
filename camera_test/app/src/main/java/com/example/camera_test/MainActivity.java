aeb9edc9aa232fbaafcf342807a06f767dfa5d13
kimsiyeon <pennya6@hs.ac.kr>
2022-03-08 14:15:29 +0900

    camera setting finish and permission error clear

--- camera_test/app/src/main/java/com/example/camera_test/MainActivity.java
+++ camera_test/app/src/main/java/com/example/camera_test/MainActivity.java
@@ -2,35 +2,144 @@ package com.example.camera_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-import com.example.camera_test.databinding.ActivityMainBinding;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Collections;
+import java.util.List;
 
-    // Used to load the 'camera_test' library on application startup.
+import static android.Manifest.permission.CAMERA;
+
+public class MainActivity extends AppCompatActivity
+        implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private int m_Camidx = 0;//front : 1, back : 0
+    private CameraBridgeViewBase m_CameraView;
+
+    private Mat matInput;
+
+    private static final int CAMERA_PERMISSION_CODE = 200;
+    private static final String TAG = "opencv";
+
+    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("camera_test");
     }
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        m_CameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
+        m_CameraView.setVisibility(SurfaceView.VISIBLE);
+        m_CameraView.setCvCameraViewListener(this);
+        m_CameraView.setCameraIndex(m_Camidx);
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'camera_test' native library,
-     * which is packaged with this application.
-     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        boolean _Permission = true; //변수 추가
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){//최소 버전보다 버전이 높은지 확인
+            if(checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_CODE);
+                _Permission = false;
+            }
+        }
+        if(_Permission){
+            //여기서 카메라뷰 받아옴
+            onCameraPermissionGranted();
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            m_LoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (m_CameraView != null)
+            m_CameraView.disableView();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (m_CameraView != null)
+            m_CameraView.disableView();
+    }
+
+    private BaseLoaderCallback m_LoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    m_CameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    protected void onCameraPermissionGranted() {
+        List<? extends CameraBridgeViewBase> cameraViews = getCameraViewList();
+        if (cameraViews == null) {
+            return;
+        }
+        for (CameraBridgeViewBase cameraBridgeViewBase: cameraViews) {
+            if (cameraBridgeViewBase != null) {
+                cameraBridgeViewBase.setCameraPermissionGranted();
+            }
+        }
+    }
+
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(m_CameraView);
+    }
+
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        matInput = inputFrame.rgba();
+        return matInput;
+    }
 }
