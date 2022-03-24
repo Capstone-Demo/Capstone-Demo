e04788c167a8558584b12e3f5fba9d15cdbf3056
kimsiyeon <pennya6@hs.ac.kr>
2022-03-24 23:58:19 +0900

    preview error check

--- Basiccode/app/src/main/java/com/example/basiccode/MainActivity.java
+++ Basiccode/app/src/main/java/com/example/basiccode/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.camera.core.AspectRatio;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.core.internal.utils.ImageUtil;
@@ -20,6 +21,10 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -28,7 +33,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
@@ -72,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                     bindPreview();
+                    bindImageCapture();
                 }
             }
         });
@@ -86,25 +96,72 @@ public class MainActivity extends AppCompatActivity {
         recogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                imageCapture.takePicture(ContextCompat.getMainExecutor(MainActivity.this), new ImageCapture.OnImageCapturedCallback() {
-//                    @Override
-//                    public void onCaptureSuccess(@NonNull ImageProxy image){
-//
-//                        @SuppressLint({"UnsafeExperimentalUsageError", "UnsafeOptInUsageError"})
-//                        Image mediaImage=image.getImage();
-//                        //Bitmap bitmap = ImageUtil.mediaImageToBitmap(mediaImage);
-//                        //imageView.setImageBitmap(bitmap);
-//
-//                        super.onCaptureSuccess(image);
-//                    }
-//                });
-                takePicture();
-            }
+                //캡처된 이미지를 제공된 파일 위치에 저장
+                imageCapture.takePicture(ContextCompat.getMainExecutor(MainActivity.this),
+                        new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    public void onCaptureSuccess(@NonNull ImageProxy image){
+                        @SuppressLint({"UnsafeExperimentalUsageError", "UnsafeOptInUsageError"})
+                        Image mediaImage=image.getImage();
+                        Bitmap bitmap = mediaImageToBitmap(mediaImage);
+                        Log.d("MainActivity", Integer.toString(bitmap.getWidth())); //4128
+                        Log.d("MainActivity", Integer.toString(bitmap.getHeight())); //3096
+                        imageView.setImageBitmap(bitmap);
+                        super.onCaptureSuccess(image);
+                    }
+                });
+                //takePicture();
+
+            };
         });
     }
+    public static Bitmap mediaImageToBitmap(Image mediaImage) {
+        byte[] byteArray = mediaImageToByteArray(mediaImage);
+        Bitmap bitmap = null;
+        if (mediaImage.getFormat() == ImageFormat.JPEG) {
+            bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        }
+        else if (mediaImage.getFormat() == ImageFormat.YUV_420_888) {
+            YuvImage yuvImage = new YuvImage(byteArray, ImageFormat.NV21, mediaImage.getWidth(), mediaImage.getHeight(), null);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 100, out);
+            byte[] imageBytes = out.toByteArray();
+            bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        }
+
+        return bitmap;
+    }
+    public static byte[] mediaImageToByteArray(Image mediaImage) {
+// Converting YUV_420_888 data to YUV_420_SP (NV21).
+        //https://developer.android.com/reference/android/media/Image.html#getFormat()
+        //https://developer.android.com/reference/android/graphics/ImageFormat#JPEG
+        //https://developer.android.com/reference/android/graphics/ImageFormat#YUV_420_888
+        byte[] byteArray = null;
+        if (mediaImage.getFormat() == ImageFormat.JPEG) {
+            ByteBuffer buffer0 = mediaImage.getPlanes()[0].getBuffer();
+            buffer0.rewind();
+            int buffer0_size = buffer0.remaining();
+            byteArray = new byte[buffer0_size];
+            buffer0.get(byteArray, 0, buffer0_size);
+        }
+        else if (mediaImage.getFormat() == ImageFormat.YUV_420_888) {
+            ByteBuffer buffer0 = mediaImage.getPlanes()[0].getBuffer();
+            ByteBuffer buffer2 = mediaImage.getPlanes()[2].getBuffer();
+            int buffer0_size = buffer0.remaining();
+            int buffer2_size = buffer2.remaining();
+            byteArray = new byte[buffer0_size + buffer2_size];
+            buffer0.get(byteArray, 0, buffer0_size);
+            buffer2.get(byteArray, buffer0_size, buffer2_size);
+        }
+
+        return byteArray;
+    }
+
     void bindPreview(){
         previewView.setScaleType(PreviewView.ScaleType.FIT_CENTER);
-        CameraSelector cameraSelector=new CameraSelector.Builder().requireLensFacing(lensFacing).build();
+        CameraSelector cameraSelector=new CameraSelector.Builder()
+                .requireLensFacing(lensFacing)
+                .build();
         Preview preview = new Preview.Builder()
                 .setTargetAspectRatio(AspectRatio.RATIO_4_3) //디폴트 표준 비율
                 .build();
@@ -112,16 +169,16 @@ public class MainActivity extends AppCompatActivity {
 
         processCameraProvider.bindToLifecycle(this, cameraSelector, preview);
     }
+
     void bindImageCapture() {
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(lensFacing)
                 .build();
         imageCapture = new ImageCapture.Builder()
                 .build();
-
+        //bindToLifecycle은 Camera객체를 반환
         processCameraProvider.bindToLifecycle(this, cameraSelector, imageCapture);
     }
-
 
     @Override
     protected void onPause() {
@@ -132,15 +189,21 @@ public class MainActivity extends AppCompatActivity {
     //사진찍기
     public void takePicture(){
         Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //앱이 처리할 수 없는 인텐트 사용시 앱 비정상 종료
+        //null이 아닌 경우에만 인텐트 사용가능
         if(intent.resolveActivity(getPackageManager())!=null){
             startActivityForResult(intent,REQUEST_IMAGE_CODE);
         }
     }
 
+    //미리보기 이미지 가져오기
+    //onActivityResult에 전달된 intent의 "data"키 아래 extras에 작은 bitmap으로 사진 인토딩
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
         super.onActivityResult(requestCode,resultCode,data);
         if(requestCode==REQUEST_IMAGE_CODE&&resultCode==RESULT_OK){
+            //Bundle : 여러가지 타입의 값을 저장하는 Map 클래스
             Bundle extras=data.getExtras();
             Bitmap bitmap=(Bitmap) extras.get("data");
             imageView.setImageBitmap(bitmap);
