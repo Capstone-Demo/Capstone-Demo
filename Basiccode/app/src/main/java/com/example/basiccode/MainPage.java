2d51bd05730da8a49b48ff63f26769845cf8910c
kimsiyeon <pennya6@hs.ac.kr>
2022-04-04 00:07:40 +0900

    방문자 페이지 생성중, 기본적인 흐름들만 구축

---
+++ Basiccode/app/src/main/java/com/example/basiccode/MainPage.java
@@ -0,0 +1,38 @@
+package com.example.basiccode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPage extends AppCompatActivity {
+
+    Button visitbutton;
+    Button dailybutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstpage);
+
+        visitbutton=findViewById(R.id.visitbutton);
+        dailybutton=findViewById(R.id.dailybutton);
+
+        visitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),VisitPayPage.class);
+                startActivity(intent);
+            }
+        });
+        dailybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Basiccode/app/src/main/java/com/example/basiccode/VisitPayPage.java
@@ -0,0 +1,29 @@
+package com.example.basiccode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VisitPayPage extends AppCompatActivity {
+    Button resultbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.visit_pay);
+        resultbutton=findViewById(R.id.resultbutton);
+        TextView textView=findViewById(R.id.textView2);
+        //textView.setText(editT);
+
+        resultbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),VisitPayPageResult.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
