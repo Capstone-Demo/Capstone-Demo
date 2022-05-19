d87a7c0f153c6d9ce827fb1165324ca9b0a7fb4f
kimsiyeon <pennya6@hs.ac.kr>
2022-05-19 13:07:58 +0900

    카메라 인식 오류 해결

--- app/src/main/java/com/example/basiccode/ExamRequest.java
+++ app/src/main/java/com/example/basiccode/ExamRequest.java
@@ -11,7 +11,8 @@ import java.util.Map;
 public class ExamRequest extends StringRequest {
 
     // 서버 URL 설정 ( PHP 파일 연동 )
-    final static private String URL = "http://175.119.82.99/Exam.php";
+    final static private String URL = "http://192.168.0.2/Exam.php";
+    //final static private String URL = "http://175.119.82.99/Exam.php";
     private Map<String, String> map;
 
     //중복검사
