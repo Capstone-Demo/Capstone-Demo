8482447175225027d9354ca5f33c3994bd317556
kimsiyeon <pennya6@hs.ac.kr>
2022-06-04 21:11:08 +0900

    app 메인 이미지 수정

--- app/src/main/java/com/example/basiccode/MypageRequest.java
+++ app/src/main/java/com/example/basiccode/MypageRequest.java
@@ -11,10 +11,10 @@ import java.util.Map;
 public class MypageRequest extends StringRequest {
 
     // 서버 URL 설정 ( PHP 파일 연동 )
-    final static private String URL = "http://192.168.2.16/mypage.php";
-    final static private String URL2 = "http://192.168.2.16/mycharge.php";
-    final static private String URL3 = "http://192.168.2.16/car.php";
-    final static private String URL4 = "http://192.168.2.16/myreserve.php";
+    final static private String URL = "http://192.168.35.21/mypage.php";
+    final static private String URL2 = "http://192.168.35.21/mycharge.php";
+    final static private String URL3 = "http://192.168.35.21/car.php";
+    final static private String URL4 = "http://192.168.35.21/myreserve.php";
     private Map<String, String> map;
 
 
