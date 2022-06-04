61c631fe338b3d0757e561bb356f77336072c59a
khg339 <khg33333339@gmail.com>
2022-06-04 20:47:47 +0900

    즐겨찾기등록추가

--- app/src/main/java/com/example/basiccode/DailyStarFragement.java
+++ app/src/main/java/com/example/basiccode/DailyStarFragement.java
@@ -1,19 +1,25 @@
 package com.example.basiccode;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -23,14 +29,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 //정기권 사용자 즐겨찾기 프래그먼트 페이지
 public class DailyStarFragement extends Fragment{
 
     DailyMainPage dailyMainPage;
+    DailyStarFragement dailyStarFragement;
     ListView listView;
     StarAdapter starAdapter;
-    DailyMainFragment dailyMainFragment;
+    AlertDialog dialog;
+
+    ArrayList<String> college_items = new ArrayList<>(Arrays.asList("전체 주차장 목록"));
+    String college_name;
 
 
     @Override
@@ -100,6 +113,7 @@ public class DailyStarFragement extends Fragment{
             @Override
             public void onClick(View view) {
                 System.out.println("즐겨찾기 등록 버튼 클릭");
+                popUpXml(user_id);
             }
         });
 
@@ -107,6 +121,114 @@ public class DailyStarFragement extends Fragment{
     }
 
     public void popUpXml(int user_id){
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View view = inflater.inflate(R.layout.star_add, null);
+        Spinner sp_star_college = view.findViewById(R.id.sp_star_college);
+
+        //college값 불러오기
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                try {
+                    System.out.println("hongchul" + response);
+                    JSONObject jsonObject = new JSONObject( response );
+
+                    JSONArray jsonArray =  jsonObject.getJSONArray("response");
+
+                    int length = jsonArray.length();
+                    if (length>0) { // 즐겨찾기 등록 가능한 주차장이 있는 경우
+                        college_items.clear();
+                        college_items.add("전체 주차장 목록");
+
+                        for(int i=0;i<length; i++){
+                            JSONObject item = jsonArray.getJSONObject(i);
+
+                            String college_name = item.getString("college_name");
+                            college_items.add(college_name);
+                        }
+
+                    } else { //없는 경우
+                        college_items.set(0, "이용 가능한 주차장이 없습니다.");
+                        return;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        ReserveRequest reserveRequest = new ReserveRequest(responseListener);
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        queue.add(reserveRequest);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, college_items);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        sp_star_college.setAdapter(adapter);
+
+        //college spineer item 클릭 시
+        sp_star_college.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                college_name = adapter.getItem(position);
+                System.out.println("입력될 주차장: " + college_name);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("즐겨찾기 등록").setView(view);
+
+
+        builder.setNegativeButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(dialogInterface != null){
+
+                    //DB에 입력
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                System.out.println("hongchul" + response);
+                                JSONObject jsonObject = new JSONObject( response );
+                                boolean success=jsonObject.getBoolean("success");
+
+                                if(success) { //즐겨찾기 등록 성공시
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                    dialog = builder.setMessage("등록이 완료되었습니다.")
+                                            .setNegativeButton("확인", null)
+                                            .create();
+                                    dialog.show();
+                                } else{
+                                    AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+                                    dialog=builder.setMessage("이미 등록된 주차장입니다.")
+                                            .setNegativeButton("확인",null)
+                                            .create();
+                                    dialog.show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    StarRequest starRequest = new StarRequest(user_id, college_name, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(getActivity());
+                    queue.add(starRequest);
+
+
+                    dialog.dismiss();
+                }
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
 
     }
 }
--- app/src/main/java/com/example/basiccode/StarRequest.java
+++ app/src/main/java/com/example/basiccode/StarRequest.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class StarRequest extends StringRequest {
     // 서버 URL 설정 ( PHP 파일 연동 )
     final static private String URL = "http://192.168.2.16/bookmark.php";
+    final static private String URL2 = "http://192.168.2.16/bookmark_plus.php";
     private Map<String, String> map;
 
 
@@ -19,6 +20,15 @@ public class StarRequest extends StringRequest {
 
         map = new HashMap<>();
         map.put("user_id",user_id + "");
+
+    }
+
+    public StarRequest(int user_id, String college_name, Response.Listener<String> listener) {
+        super(Request.Method.POST, URL2, listener, null);
+
+        map = new HashMap<>();
+        map.put("user_id",user_id + "");
+        map.put("college_name",college_name);
 
     }
 
