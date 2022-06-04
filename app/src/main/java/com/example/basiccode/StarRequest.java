c9dfe9b0ec5354d5550340b117970a47b30c582c
kimsiyeon <pennya6@hs.ac.kr>
2022-06-04 20:59:43 +0900

    conflict 해결중

--- app/src/main/java/com/example/basiccode/StarRequest.java
+++ app/src/main/java/com/example/basiccode/StarRequest.java
@@ -10,8 +10,10 @@ import java.util.Map;
 
 public class StarRequest extends StringRequest {
     // 서버 URL 설정 ( PHP 파일 연동 )
-    final static private String URL = "http://192.168.35.21/bookmark.php";
+    final static private String URL = "http://192.168.2.16/bookmark.php";
+    final static private String URL2 = "http://192.168.2.16/bookmark_plus.php";
     private Map<String, String> map;
+
 
     public StarRequest(int user_id, Response.Listener<String> listener) {
         super(Request.Method.POST, URL, listener, null);
@@ -20,6 +22,16 @@ public class StarRequest extends StringRequest {
         map.put("user_id",user_id + "");
 
     }
+
+    public StarRequest(int user_id, String college_name, Response.Listener<String> listener) {
+        super(Request.Method.POST, URL2, listener, null);
+
+        map = new HashMap<>();
+        map.put("user_id",user_id + "");
+        map.put("college_name",college_name);
+
+    }
+
     @Override
     protected Map<String, String> getParams() throws AuthFailureError {
         return map;
